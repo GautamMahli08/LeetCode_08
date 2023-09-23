949e92789e4e17fe7c7bb1e413909285d59a08cf
GautamMahli08 <140971352+GautamMahli08@users.noreply.github.com>
2023-09-23 22:27:43 +0530

    Add files via upload
    One of the Easiest problem on leetcode.

---
+++ Palindrome_INT_Value_Leetcode-Problem.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+bool Is_Pal(int x)
+{
+    if(x<0)
+        return false;
+    long n=x;
+    long reverse=0;
+
+        int lastdigit,revers;
+        while(x>0){
+        lastdigit =x%10;
+        revers=(revers*10)+lastdigit;
+        x/=10;
+        }
+    if(n==revers){
+        return true;
+    }
+    else
+        return false;
+
+}
+int main()
+{
+    cout<< "Is it Palindrome = " <<Is_Pal(121) << endl;
+        cout<< "Is it Palindrome = " <<Is_Pal(-121) << endl;
+        cout<< "Is it Palindrome = " <<Is_Pal(10) << endl;
+    }
