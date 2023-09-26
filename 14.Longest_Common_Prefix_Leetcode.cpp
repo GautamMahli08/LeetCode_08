020fe486cd0a74e3dffbc8ddba8735761f2f04df
GautamMahli08 <140971352+GautamMahli08@users.noreply.github.com>
2023-09-26 22:27:29 +0530

    Update 14.Longest_Common_Prefix_Leetcode.cpp

--- 14.Longest_Common_Prefix_Leetcode.cpp
+++ 14.Longest_Common_Prefix_Leetcode.cpp
@@ -1,3 +1,36 @@
+//Find here Leetcode_code As Well As cpp_code
+
+
+----------------------------Leetcode-------------------------------------
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int count =0;
+        string str;
+        string min_=*min_element(strs.begin(),strs.end());
+        int length=min_.size();
+
+        for(int i=0 ; i<strs.size() ; i++){
+            int len=strs[i].length();
+            if(len<length)
+                length=len;
+                str=strs[i];
+        }
+
+        for(int i=0 ; i<length ; i++){
+            for(int j=0 ; j<strs.size() ; j++){
+                if(strs[j][i]!=str[i])
+                return str.substr(0,count);
+            }
+            count++;
+        }
+        return str.substr(0,count);
+    }
+};
+
+
+
+--------------------------cpp-----------------------------------------------
 #include <iostream>
 #include <vector>
 #include <bits/stdc++.h>
