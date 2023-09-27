bd7cb9117116c2e18d9cf07ae0cb2220ade7dc3f
GautamMahli08 <140971352+GautamMahli08@users.noreply.github.com>
2023-09-28 02:35:53 +0530

    Update 20_Valid Parentheses_Leetcode.cpp

--- 20_Valid Parentheses_Leetcode.cpp
+++ 20_Valid Parentheses_Leetcode.cpp
@@ -1,3 +1,39 @@
+Find here Leetcode-code As Well As cpp-code
+
+------------------------Leetcode----------------------------------
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for(int i=0; i<s.size(); i++) 
+         {
+            if(s[i]=='('||s[i]=='['||s[i]=='{')
+                st.push(s[i]);
+
+
+           else if(!st.empty())
+           {
+                char c=st.top();
+                if(c=='(' && s[i]==')')
+                      st.pop();
+                else if(c=='[' && s[i]==']')
+                      st.pop();
+                else if(c=='{' && s[i]=='}')
+                      st.pop();                
+            }
+            else
+                return false;
+        }  
+        if(!st.empty())
+            return false;
+        return true;
+}
+};
+
+
+
+--------------------------cpp-------------------------------------
 #include <iostream>
 #include <stack>
 using namespace std;
@@ -22,7 +58,6 @@ int main()
                       st.pop();
                 else if(c=='{' && s[i]=='}')
                       st.pop();
-
             }
     else
         return false;
@@ -31,7 +66,6 @@ int main()
     if(!st.empty())
         return false;
     return true;
-
 }
 
 
