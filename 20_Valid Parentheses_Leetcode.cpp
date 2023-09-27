04aba5997f92ddbb44a04d5e7644b7ef7c6b35ae
GautamMahli08 <140971352+GautamMahli08@users.noreply.github.com>
2023-09-28 02:33:31 +0530

    Add files via upload

---
+++ 20_Valid Parentheses_Leetcode.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    string s ="(]";
+    stack<char>st;
+
+   if(s[0]==')'||s[0]==']'||s[0]=='}')
+            return false;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+                st.push(s[i]);
+
+               else if(!st.empty())
+              {
+                char c=st.top();
+                if(c=='(' && s[i]==')')
+                      st.pop();
+                else if(c=='[' && s[i]==']')
+                      st.pop();
+                else if(c=='{' && s[i]=='}')
+                      st.pop();
+
+            }
+    else
+        return false;
+    }
+
+    if(!st.empty())
+        return false;
+    return true;
+
+}
+
+
+
+
+
+
+
+
+
+
