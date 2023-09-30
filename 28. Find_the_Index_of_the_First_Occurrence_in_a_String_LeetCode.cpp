8c322eeaaba0038fabb5c347b2f97260f9b3c1d5
GautamMahli08 <140971352+GautamMahli08@users.noreply.github.com>
2023-10-01 00:09:31 +0530

    Add files via upload

---
+++ 28. Find_the_Index_of_the_First_Occurrence_in_a_String_LeetCode.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int check_string(string haystack, string needle)
+{
+
+    for(int i=0 ;i<haystack.size() ; i++)
+    {
+        for(int j=0 ; j<needle.size() ; j++)
+        {
+            if(haystack[i+j]!=needle[j]){
+            break;
+            }
+
+
+            if(j==(needle.size()-1)){
+                return i; }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    string sh="leetcode";
+    string ch="leeto";
+
+    int value = check_string(sh,ch);
+    cout<<"Found at index = "<<value;
+    return 0;
+}
