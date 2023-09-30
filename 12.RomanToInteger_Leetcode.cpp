0047e858371e8f282c92bacc61e0213425870c0f
GautamMahli08 <140971352+GautamMahli08@users.noreply.github.com>
2023-10-01 00:14:24 +0530

    Update 12.RomanToInteger_Leetcode.cpp

--- 12.RomanToInteger_Leetcode.cpp
+++ 12.RomanToInteger_Leetcode.cpp
@@ -1,3 +1,37 @@
+//Find Here Leetcode-Code As Well As cpp-Code
+
+
+--------------------------- Leetcode ----------------------------------
+
+    class Solution {
+public:
+    int romanToInt(string s) {
+       unordered_map<char,int> My_map {
+        {'I',1},
+        {'V',5},
+        {'X',10},
+        {'L',50},
+        {'C',100},
+        {'D',500},
+        {'M',1000},
+       };
+       
+       int _val=0;
+       for(int i=0 ; i<s.size() ; i++){
+           if(My_map[s[i]]<My_map[s[i+1]])
+                _val-=My_map[s[i]];
+           else
+                _val+=My_map[s[i]];
+       }
+       return _val;
+    
+       } 
+    
+};
+
+----------------------------- cpp ------------------------------------- 
+    
+
 #include <iostream>
 #include <unordered_map>
 using namespace std;
