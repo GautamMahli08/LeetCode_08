1246e157d0044df60329280e0a7e47a4d7cea52e
GautamMahli08 <140971352+GautamMahli08@users.noreply.github.com>
2023-10-01 00:11:45 +0530

    Update 28. Find_the_Index_of_the_First_Occurrence_in_a_String_LeetCode.cpp

--- 28. Find_the_Index_of_the_First_Occurrence_in_a_String_LeetCode.cpp
+++ 28. Find_the_Index_of_the_First_Occurrence_in_a_String_LeetCode.cpp
@@ -1,3 +1,32 @@
+//Find Here Leetcode-Code As Well As cpp-Code
+
+------------------------------ Leetcode ----------------------------------
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        for(int i=0 ; i<haystack.size() ; i++)
+        {
+            for(int j=0 ; j<needle.size() ; j++){
+                if(haystack[i+j]!=needle[j])
+                    {
+                        break;
+                    }
+
+                if(j==(needle.size()-1))
+                    {
+                        return i;
+                    }    
+            }
+        }
+        return -1;
+    }
+};
+
+
+--------------------------------- cpp ------------------------------------    
+
+    
 #include <iostream>
 #include <string>
 using namespace std;
